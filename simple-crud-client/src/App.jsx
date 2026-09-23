2977d6f587b3f83f6ffd4079a82c7315fb36bd1d
md-shakib-hassan <arefinshakibkhan@gmail.com>
2026-09-23 13:32:47 +0600

    here add crud operation code

---
+++ simple-crud-client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Form from './components/From'
+
+
+function App() {
+  
+
+  return (
+   <>
+   <Form></Form>
+   </>
+  )
+}
+
+export default App
---
+++ simple-crud-client/src/components/From.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Form = () => {
+
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        
+    }
+
+  return (
+    <div>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body" >
+                            <form action="" onSubmit={handleSubmit}>
+                                <fieldset className="fieldset" >
+                                    <label className="label">Name</label>
+                                    <input type="text" name='name' className=" input" placeholder="Name" />
+                                    <label className="label"></label>
+                                    <input type="text" name='email' className="model input" placeholder="email" />
+                                    <button className="btn btn-neutral mt-4">Submit</button>
+                                </fieldset>
+
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Form
